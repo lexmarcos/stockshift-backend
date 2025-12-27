479655cbd4ef9b21800f481a85a6e652e2debfcd
lexmarcos <marcosuel.vieira@hotmail.com>
2025-12-27 19:25:23 -0300

    feat: add JWT configuration properties

---
+++ src/main/java/br/com/stockshift/config/JwtProperties.java
@@ -0,0 +1,14 @@
+package br.com.stockshift.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+    private String secret;
+    private Long accessExpiration; // in milliseconds
+    private Long refreshExpiration; // in milliseconds
+}
