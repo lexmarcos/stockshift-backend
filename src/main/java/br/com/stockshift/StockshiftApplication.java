61ed3aad738e7b7e085f96057b8f4ce59d16243e
lexmarcos <marcosuel.vieira@hotmail.com>
2025-12-27 17:27:53 -0300

    chore: create package structure

---
+++ src/main/java/br/com/stockshift/StockshiftApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockshiftApplication.class, args);
+	}
+
+}
