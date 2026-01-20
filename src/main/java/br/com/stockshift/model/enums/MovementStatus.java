53ef62d3501080500be17301af04391497b2ea85
lexmarcos <marcosuel.vieira@hotmail.com>
2026-01-20 17:41:18 -0300

    feat: add COMPLETED_WITH_DISCREPANCY status to MovementStatus enum

--- src/main/java/br/com/stockshift/model/enums/MovementStatus.java
+++ src/main/java/br/com/stockshift/model/enums/MovementStatus.java
@@ -4,5 +4,6 @@ public enum MovementStatus {
     PENDING,    // Created but not executed
     IN_TRANSIT, // Product left origin, not arrived at destination
     COMPLETED,  // Movement completed
+    COMPLETED_WITH_DISCREPANCY, // Movement completed but with missing items
     CANCELLED   // Movement cancelled
 }
