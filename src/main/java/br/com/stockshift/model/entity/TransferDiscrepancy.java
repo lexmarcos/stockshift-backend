aa99e68f0d06afc280841ac23099a0b66ae88c3f
lexmarcos <marcosuel.vieira@hotmail.com>
2026-01-20 17:44:49 -0300

    feat: add TransferDiscrepancy entity

---
+++ src/main/java/br/com/stockshift/model/entity/TransferDiscrepancy.java
@@ -0,0 +1,33 @@
+package br.com.stockshift.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "transfer_discrepancies")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferDiscrepancy extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transfer_validation_id", nullable = false)
+    private TransferValidation transferValidation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_movement_item_id", nullable = false)
+    private StockMovementItem stockMovementItem;
+
+    @Column(name = "expected_quantity", nullable = false)
+    private Integer expectedQuantity;
+
+    @Column(name = "received_quantity", nullable = false)
+    private Integer receivedQuantity;
+
+    @Column(name = "missing_quantity", nullable = false)
+    private Integer missingQuantity;
+}
