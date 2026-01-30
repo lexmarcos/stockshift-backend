68f0a50a344a8922ee651d5976ca958adfcdbf11
lexmarcos <marcosuel.vieira@hotmail.com>
2026-01-29 22:32:04 -0300

    fix(transfer): add missing index for origin_batch_id

--- src/main/resources/db/migration/V29__add_batch_origin_fields.sql
+++ src/main/resources/db/migration/V29__add_batch_origin_fields.sql
@@ -4,3 +4,4 @@ ALTER TABLE batches ADD COLUMN origin_batch_id UUID REFERENCES batches(id) ON DE
 
 -- Index for finding batches created from transfers
 CREATE INDEX idx_batches_origin_transfer ON batches(origin_transfer_id) WHERE origin_transfer_id IS NOT NULL;
+CREATE INDEX idx_batches_origin_batch ON batches(origin_batch_id) WHERE origin_batch_id IS NOT NULL;
