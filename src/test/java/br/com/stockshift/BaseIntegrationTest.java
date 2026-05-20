303139967fa4dcb9d4173373e56fba714f77c9d5
lexmarcos <marcosuel.vieira@hotmail.com>
2026-05-20 16:51:43 -0300

    test: update testcontainers for docker compatibility

--- src/test/java/br/com/stockshift/BaseIntegrationTest.java
+++ src/test/java/br/com/stockshift/BaseIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -23,12 +23,12 @@ public abstract class BaseIntegrationTest {
 
     // Singleton container shared across all test classes
     @SuppressWarnings("resource")
-    static PostgreSQLContainer<?> postgres;
+    static PostgreSQLContainer postgres;
     @SuppressWarnings("resource")
     static org.testcontainers.containers.GenericContainer<?> redis;
 
     static {
-        postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+        postgres = new PostgreSQLContainer("postgres:16-alpine")
                 .withDatabaseName("stockshift_test")
                 .withUsername("test")
                 .withPassword("test");
